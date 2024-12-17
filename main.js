998068e267cbc1509029e05025e2aa057a7be32a
edieyelovesye <135151571+edieyelovesye@users.noreply.github.com>
2024-12-17 06:01:15 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const hamburger = document.querySelector('.hamburger');
+    const menu = document.querySelector('.fullscreen-menu');
+    hamburger.classList.toggle('active');
+    menu.classList.toggle('show');
+}
